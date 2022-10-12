c56c9d7b0c3528465bd01368f068cbad924cc2c0
FolashadeOmoloja <omolojashade@gmail.com>
2022-10-12 10:46:02 +0100

    new commit

--- set-and-maps/main.js
+++ set-and-maps/main.js
@@ -1,25 +1,36 @@
-const countries = new Set()
-console.log(countries)
+// const countries = new Set()
+// console.log(countries)
 
-const languages = [
-    'English',
-    'Finnish',
-    'Yoruba',
-    'French',
-    'Spanish',
-    'English',
-    'Germany',
-  ]
-  const langSet = new Set(languages)
-  console.log(langSet)
+// const languages = [
+//     'English',
+//     'Finnish',
+//     'Yoruba',
+//     'French',
+//     'Spanish',
+//     'English',
+//     'Germany',
+//   ]
+//   const langSet = new Set(languages)
+//   console.log(langSet)
 
   
-const counts = []
-const count = {}
+// const counts = []
+// const count = {}
 
-for (const l of langSet) {
-  const filteredLang = languages.filter((lng) => lng === l)
-  console.log(filteredLang) // ["English", "English", "English"]
-  counts.push({ lang: l, count: filteredLang.length })
-}
-console.log(counts)
+// for (const l of langSet) {
+//   const filteredLang = languages.filter((lng) => lng === l)
+//   console.log(filteredLang) // ["English", "English", "English"]
+//   counts.push({ lang: l, count: filteredLang.length })
+// }
+// console.log(counts)
+
+
+const arr = [2,5,6,7,8]
+ const newArr = []
+
+ let i = 0
+ while(i<arr.length){
+   newArr.push((i * 3))
+   i++
+  }
+  console.log(newArr)
