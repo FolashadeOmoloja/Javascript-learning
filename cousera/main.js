4fd933633b922ad1885c62c5dcc61968a849292c
FolashadeOmoloja <omolojashade@gmail.com>
2022-10-13 23:14:19 +0100

    some changes

--- cousera/main.js
+++ cousera/main.js
@@ -34,3 +34,30 @@ function styleAndCelebrate(color, background, fontSize, txt, reason) {
 }
 // Call styleAndCelebrate
 styleAndCelebrate('ef7c8e','fae8e0','53px','You made it!','champions')
+
+function meal(animal) {
+    animal.food = animal.food + 10;
+    console.log(animal.food);
+}
+
+var dog = {
+    food: 10
+};
+meal(dog);
+meal(dog);
+
+console.log(dog.food);
+
+function two() {
+    return 2;
+}
+
+function one() {
+    return 1;
+}
+
+function calculate(initialValue, incrementValue) {
+    return initialValue() + incrementValue() + incrementValue();
+}
+
+console.log(calculate(two, one));
